f2f6afa4bc67d816815f0af79ef4d06e7b3f2e3b
Arpad Toth <arpad.toth@umwerk.com>
2022-06-28 21:57:39 +0300

    Update to MVC

--- xo/game.js
+++ xo/game.js
@@ -1,20 +1,34 @@
 
 let player = 'X';
-let buttons = document.getElementsByClassName("button");
+let board = [
+    'X', '', '',
+    '', '', '',
+    '', '', '0',
 
+]
+let buttons = document.getElementsByClassName("button");
+const NOBODY = '';
+
+
+function updateBoard() {
+   for (let i = 0; i < board.length; i++) {
+      let item = board[i];
+      let button = buttons[i];
+      button.textContent = item;
+   }
+}
+
+
+document.getElementById("reset").addEventListener("click", () => {
+    board = new Array(9).fill('');
+    updateBoard();
+}); 
 
 for (let i = 0; i < buttons.length; i++) {
-   
-   let button = buttons[i];
-   console.log(button);
-   
-   button.addEventListener("click", () => {
-      button.classList.add("clicked");
-      button.textContent = player;
-      if (player === 'X')
-          player = '0';
-      else 
-          player = 'X';
+   buttons[i].addEventListener("click", () => {
+       board[i] = player;
+       updateBoard();
    }); 
 }
 
+updateBoard();
